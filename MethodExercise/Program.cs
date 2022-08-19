870f696acc1e0f48fb3e8e45fc3f9a92504a156f
robertsteele7 <atpcycle7@gmail.com>
2022-08-19 13:22:08 -0500

    first commit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string favoriteColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            string favoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            string favoriteBand = Console.ReadLine();
+
+            Console.WriteLine($"There was a man named {userName} favorite color is {favoriteColor}");
+            Console.WriteLine($"Likes {favoriteAnimal} and listens to {favoriteBand}");
+
+            Console.WriteLine("Give me a number to add");
+            int num1 =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the first one");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = Sum(num1, num2);
+            Console.WriteLine($"The sum is: {sum}");
+
+            Console.WriteLine("Give me a number to add");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to multiply to the first one");
+            num2 = int.Parse(Console.ReadLine());
+
+            int product = Multiply(num1, num2);
+            Console.WriteLine($"The product is: {product}");
+        }
+        public static int Sum(params int[] list)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                sum = sum + list[i];
+            }
+
+            return sum;
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
